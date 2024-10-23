ef4a3511c86e98d86308b9e544e3f18020db65bf
MarianaBernal <mariana_bernal@ciencias.unam.mx>
2024-10-23 15:32:19 -0600

    clases product implementadas

---
+++ dwf-practica4/src/app/modules/product/component/product/product-image/product-image-component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-product-image',
+    standalone: true,
+    imports: [],
+    templateUrl: './product-image.component.css'
+})
+export class ProductImageComponent {
+    
+}
